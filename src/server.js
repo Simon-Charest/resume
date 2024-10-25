1a49df4a2ea4e617e43d0fabaed7a6f1e5b414a7
Simon Charest <scharest@vigiteck.com>
2024-10-25 15:22:06 -0400

    Automated deployment

--- src/server.js
+++ src/server.js
@@ -10,20 +10,16 @@ function main() {
     const cookieParser = require('cookie-parser');
     const app = express();
     const resumePath = path.join(__dirname, '..', 'data', 'resume.json');
+    const iconsDir = path.join(__dirname, '..', 'assets', 'icons');
+    const imagesDir = path.join(__dirname, '..', 'assets', 'images');
 
     app.use(cookieParser());
 
     // Middleware to set the language
     app.use((req, res, next) => {
-        // Set default language
         const lang = req.query.lang || req.cookies.lang || DEFAULT_LANGUAGE;
-
-        // Set cookie
         res.cookie('lang', lang, { maxAge: 900000, httpOnly: true });
-
-        // Set the language in res.locals for use in routes
         res.locals.lang = lang;
-        
         next();
     });
 
@@ -39,15 +35,105 @@ function main() {
             if (err) {
                 console.error('Error reading JSON file:', err);
                 res.status(500).send('Internal Server Error');
-                
                 return;
             }
-
+    
             const resumeData = JSON.parse(data);
-            
-            // Render the index.ejs template and pass the resume data and content
-            res.render('index', {
-                resume: resumeData
+    
+            // Read icons directory
+            fs.readdir(iconsDir, (err, iconFiles) => {
+                if (err) {
+                    console.error('Error reading icons directory:', err);
+                    iconFiles = [];
+                }
+    
+                // Read images directory
+                fs.readdir(imagesDir, (err, imageFiles) => {
+                    if (err) {
+                        console.error('Error reading images directory:', err);
+                        imageFiles = [];
+                    }
+    
+                    // Create an object mapping filenames
+                    const icons = {};
+                    iconFiles.forEach(file => {
+                        // Use filename without extension as key
+                        const key = path.parse(file).name;
+                        
+                        // Map key to full filename
+                        icons[key] = file;
+                    });
+    
+                    const images = {};
+                    imageFiles.forEach(file => {
+                        // Use filename without extension as key
+                        const key = path.parse(file).name;
+                        
+                        // Map key to full filename
+                        images[key] = file;
+                    });
+                
+                    const calculateLength = (startDate, endDate) => {
+                        const start = new Date(startDate);
+                        const end = endDate == null ? new Date() : new Date(endDate);
+                        const yearDiff = end.getFullYear() - start.getFullYear();
+                        const monthDiff = end.getMonth() - start.getMonth();
+                        const totalMonths = yearDiff * 12 + monthDiff;
+                        const years = Math.floor(totalMonths / 12);
+                        const months = totalMonths % 12;
+
+                        return { years: years, months: months };
+                    };
+
+                    const formatLength = ({ years, months }, lang) => {
+                        let string = '';
+
+                        if (years > 0) {
+                            if (lang == 'en-ca') {
+                                string += `${years} ${years > 1 ? 'years' : 'year'}`;
+                            }
+
+                            else {
+                                string += `${years} ${years > 1 ? 'ans' : 'an'}`;
+                            }
+                        }
+
+                        if (months > 0) {
+                            if (string.length > 0) {
+                                if (lang == 'en-ca') {
+                                    string += ' and ';
+                                }
+
+                                else {
+                                    string += ' et ';
+                                }
+                            }
+
+                            if (lang == 'en-ca') {
+                                string += `${months} ${months > 1 ? 'months' : 'month'}`;
+                            }
+
+                            else {
+                                string += `${months} ${months > 1 ? 'mois' : 'mois'}`;
+                            }
+                        }
+
+                        if (string.length === 0) {
+                            string = '0 months';
+                        }
+
+                        return string;
+                    };
+                    
+                    // Render the index.ejs template and pass the resume data and file lists
+                    res.render('index', {
+                        resume: resumeData,
+                        icons,
+                        images,
+                        calculateLength,
+                        formatLength
+                    });
+                });
             });
         });
     });
