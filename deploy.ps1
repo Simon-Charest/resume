5075a7943df3a3313c787578d2ff24ffb02b78eb
Simon Charest <scharest@vigiteck.com>
2024-10-24 22:34:20 -0400

    Dev

--- deploy.ps1
+++ deploy.ps1
@@ -1,4 +1,7 @@
+$sudoPassword = "sC941gsv#"
+
 git add *
 git commit -m "Dev"
 git push
-ssh scharest@slcti.8bit.ca "cd ~/source/resume && git pull && sudo systemctl daemon-reload && sudo systemctl restart resume.service"
+
+ssh scharest@slcti.8bit.ca "cd ~/source/resume && git pull && echo $sudoPassword | sudo -S systemctl daemon-reload && echo $sudoPassword | sudo -S systemctl restart resume.service"
