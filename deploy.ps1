f776d61046e0419c84d412763f3d3cb1d0f2d79f
Simon Charest <scharest@vigiteck.com>
2024-10-24 22:58:43 -0400

    Dev

--- deploy.ps1
+++ deploy.ps1
@@ -1,4 +1,4 @@
 git add *
 git commit -m "Dev"
 git push
-ssh scharest@slcti.8bit.ca "cd ~/source/resume && git pull && sudo systemctl daemon-reload && sudo systemctl restart resume.service"
+ssh simonchares@slcti.8bit.ca "cd ~/source/resume && git pull && sudo systemctl daemon-reload && sudo systemctl restart resume.service"
