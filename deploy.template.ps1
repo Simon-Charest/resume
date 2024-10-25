b267251fe77c3cc8b57d429b0f9f59b5154e99dd
Simon Charest <scharest@vigiteck.com>
2024-10-25 07:30:56 -0400

    Redacting

--- deploy.template.ps1
+++ deploy.template.ps1
@@ -30,4 +30,4 @@ Usage: .\deploy.ps1
 git add *
 git commit -m "Automated deployment"
 git push
-ssh scharest@slcti.8bit.ca "cd ~/source/resume && git pull && sudo systemctl daemon-reload && sudo systemctl restart resume.service"
+ssh USERNAME@DOMAIN "cd DIRECTORY && git pull && sudo systemctl daemon-reload && sudo systemctl restart resume.service"
