c41ef4d8477e79f15ff467d7a9be68765c92d69c
Simon Charest <scharest@vigiteck.com>
2024-11-06 16:59:33 -0500

    Refactoring

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,3 @@
-function convertPlainTextLinksToHtml(text) {
-    /* Convert plain text links to HTML */
-
-    // Capture URLs without trailing characters
-    const urlRegex = /(?<!<a href=")https?:\/\/[^\s,()]+/g;
-    
-    return text.replace(urlRegex, (url) => {
-        return `<a href="${url}" target="_blank" rel="noopener noreferrer">${url}</a>`;
-    });
-}
-
 function traverseAndConvert(element) {
     element.childNodes.forEach(node => {
         if (node.nodeType === Node.TEXT_NODE) {
@@ -28,5 +17,16 @@ function traverseAndConvert(element) {
         else if (node.nodeType === Node.ELEMENT_NODE) {
             traverseAndConvert(node);
         }
+    });
+}
+
+function convertPlainTextLinksToHtml(text) {
+    /* Convert plain text links to HTML */
+
+    // Capture URLs without trailing characters
+    const urlRegex = /(?<!<a href=")https?:\/\/[^\s,()]+/g;
+    
+    return text.replace(urlRegex, (url) => {
+        return `<a href="${url}" target="_blank">${url}</a>`;
     });
 }
