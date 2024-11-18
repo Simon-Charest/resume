174e37f9a952691439634f1b60644daf27454425
Simon Charest <scharest@vigiteck.com>
2024-11-18 10:51:24 -0500

    Automated deployment

--- src/server.js
+++ src/server.js
@@ -8,6 +8,7 @@ async function main() {
     const cors = require('cors');
     const express = require('express');
     const fs = require('fs').promises;
+    const http = require('http');
     const path = require('path');
     
     const assetsDir = path.join(__dirname, '..', 'assets');
@@ -160,6 +161,11 @@ async function main() {
 
     app.listen(PORT, HOSTNAME, () => {
         console.log(`Server running at ${PROTOCOL}://${HOSTNAME}:${PORT}/`);
+    });
+
+    // Create a new HTTP server to handle the challenge on port 80
+    http.createServer(app).listen(80, () => {
+        console.log('ACME challenge server running on port 80');
     });
 }
 
