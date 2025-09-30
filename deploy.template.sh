cc9e63d1388af7771e2d59db4a173bb320c0928d
Simon Charest <simoncharest@gmail.com>
2025-09-30 14:01:49 -0400

    Automated deployment

--- deploy.template.sh
+++ deploy.template.sh
@@ -14,13 +14,19 @@ git add .
 git commit -m "$MESSAGE"
 git push
 
+# Pause
+sleep 2
+
 # Remote SSH Deployment Commands
 ssh "$USER@$HOST_" 'bash -c "
 cd '"$DIRECTORY"' &&
+git fetch origin &&
 git reset --hard origin/main &&
 git pull &&
 npm install &&
-npm audit fix &&
+npm audit fix --force &&
+npm run build &&
 sudo systemctl daemon-reload &&
-sudo systemctl restart '"$COMMAND"'"
-'
+sudo systemctl restart '"$COMMAND"' &&
+sudo systemctl status '"$COMMAND"' --no-pager
+"'
